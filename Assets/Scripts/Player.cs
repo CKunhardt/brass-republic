6e2e03c4af1bc363cc585eb6d890b939af58e004
Clinton Kunhardt <24also20@gmail.com>
2018-03-04 14:14:02 -0600

    Sprite + animations
    Added player sprite + animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	private Rigidbody2D rb2D;
+	private Animator anim;
 	private float moveSpeed = 0.1f;
 
 	// Use this for initialization
 	protected void Start () {
 		rb2D = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
 	}
 
 	protected void Move (int xDir, int yDir)
 	{
-		Vector2 newPos = new Vector2(rb2D.position.x + xDir * moveSpeed, rb2D.position.y + yDir * moveSpeed);
-		rb2D.MovePosition(newPos);
+		Vector2 newPos = new Vector2(xDir, yDir);
+		rb2D.MovePosition(rb2D.position + newPos * moveSpeed);
 	}
 
 	
@@ -29,8 +31,14 @@ public class Player : MonoBehaviour {
 			vertical = 0;
 
 		if (horizontal != 0 || vertical != 0) {
-			Move(horizontal, vertical);
+			anim.SetBool ("isWalking", true);
+			anim.SetFloat ("input_x", horizontal);
+			anim.SetFloat ("input_y", vertical);
+			Move (horizontal, vertical);
+		} else {
+			anim.SetBool ("isWalking", false);
 		}
+
 	}
 
 }
