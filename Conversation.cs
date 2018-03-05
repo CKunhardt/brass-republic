442851a77ef06f0ea54acb0d360e17b62f5b3a4a
hushion-r <hushion_r@zoho.com>
2018-03-05 11:17:57 -0600

    Dialogue
    dialogue. small changes

--- Conversation.cs
+++ Conversation.cs
@@ -12,3 +12,47 @@ public class Conversation
 
     public Node startNode;
 }
+
+
+public void TriggerDialogue()
+{
+    FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+}
+
+
+
+public Text nameText;
+public Text dialogueText;
+
+
+
+
+
+nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+{
+    if (sentences.Count == 0)
+    {
+        EndDialogue();
+        return;
+    }
+
+    string sentence = sentences.Dequeue();
+    dialogueText.text = sentence;
+}
+
+void EndDialogue()
+{
+    Debug.Log("End of conversation.");
+}
