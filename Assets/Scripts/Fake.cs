49258de990165fe849287bbc4b514674764e66d7
hushion-r <shushion@macalester.edu>
2018-04-12 13:06:10 -0500

    Added pause/start menus
    pause > game, pause > main, main > game works.

---
+++ Assets/Scripts/Fake.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fake : MonoBehaviour {
+
+    public void Pause() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        // if Game is 2 in build and Pause is 1
+
+    }
+
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+    // current bug where when go to Pause, quickly goes to Main without clicking
+    // fixed by uncheking Pause script from PauseMenu component
+
+    public void Main() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // if Pause is 3 in build and Menu is 4
+    }
+
+    public void Game() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+        // if Pause is 3 in build and Game is 0
+    }
+
+}
