3ad14127e35a146b815a7c3d6cf9794e5bcaac75
Clinton Kunhardt <24also20@gmail.com>
2018-04-18 16:58:44 -0500

    Can get into battle system through F1
    :D

--- Assets/Scripts/Utilities/EventMessageHandler.cs
+++ Assets/Scripts/Utilities/EventMessageHandler.cs
@@ -8,19 +8,37 @@ public class EventMessageHandler : MonoBehaviour, IEventMessageHandler
 
 	public void CheckSceneEvents (string sceneName)
 	{
-		if (sceneName == "YourBedroom" && !GameManager.Instance.GSV.AwokeInRoom) {
-			ExecuteEvents.Execute<IDialogueMessageHandler> (GameManager.Instance.DMH, null, (x, y) => x.DialogueMessage_OnAwakeInBedroom ());
-			GameManager.Instance.GSV.AwokeInRoom = true;
-		} else if (sceneName == "LivingRoom" && !GameManager.Instance.GSV.EnteredLivingRoom) {
-			ExecuteEvents.Execute<IDialogueMessageHandler> (GameManager.Instance.DMH, null, (x, y) => x.DialogueMessage_OnEnterLivingRoom ());
-			GameManager.Instance.GSV.EnteredLivingRoom = true;
-		} else if (sceneName == "ParentsBedroom" && !GameManager.Instance.GSV.EnteredParentsRoom) {
-			ExecuteEvents.Execute<IDialogueMessageHandler> (GameManager.Instance.DMH, null, (x, y) => x.DialogueMessage_OnEnterParentsRoom ());
-			GameManager.Instance.GSV.EnteredParentsRoom = true;
-		} else if (sceneName == "MainRoad" && !GameManager.Instance.GSV.EnteredMainRoad) {
-			ExecuteEvents.Execute<IDialogueMessageHandler> (GameManager.Instance.DMH, null, (x, y) => x.DialogueMessage_OnEnterMainRoad ());
-			GameManager.Instance.GSV.EnteredMainRoad = true;
+		switch (sceneName) {
+		case "YourBedroom":
+			if (!GameManager.Instance.GSV.AwokeInRoom) {
+				ExecuteEvents.Execute<IDialogueMessageHandler> (GameManager.Instance.DMH, null, (x, y) => x.DialogueMessage_OnAwakeInBedroom ());
+				GameManager.Instance.GSV.AwokeInRoom = true;
+			}
+			break;
+		case "LivingRoom":
+			if (!GameManager.Instance.GSV.EnteredLivingRoom) {
+				ExecuteEvents.Execute<IDialogueMessageHandler> (GameManager.Instance.DMH, null, (x, y) => x.DialogueMessage_OnEnterLivingRoom ());
+				GameManager.Instance.GSV.EnteredLivingRoom = true;
+			}
+			break;
+		case "ParentsBedroom":
+			if (!GameManager.Instance.GSV.EnteredParentsRoom) {
+				ExecuteEvents.Execute<IDialogueMessageHandler> (GameManager.Instance.DMH, null, (x, y) => x.DialogueMessage_OnEnterParentsRoom ());
+				GameManager.Instance.GSV.EnteredParentsRoom = true;
+			}
+			break;
+		case "MainRoad":
+			if (!GameManager.Instance.GSV.EnteredMainRoad) {
+				ExecuteEvents.Execute<IDialogueMessageHandler> (GameManager.Instance.DMH, null, (x, y) => x.DialogueMessage_OnEnterMainRoad ());
+				GameManager.Instance.GSV.EnteredMainRoad = true;
+			}
+			break;
+		case "BattleSystemTest":
+			GameManager.Instance.playerObject.SetActive (false);
+			GameManager.Instance.comingFromBattle = true;
+			break;
 		}
+
 	}
 
 	public void CheckDialogueEvents (string targetName)
