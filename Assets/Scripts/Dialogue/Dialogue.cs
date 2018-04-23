6163a4bcc83701082a19b45006f0a4de1fbe870a
Clinton Kunhardt <24also20@gmail.com>
2018-04-23 16:29:00 -0500

    Icons!

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Dialogue
 {
-    public string name;
+	public string name;
+	public Sprite avatar;
 
-    [TextArea(1,4)]
-
-    
-    public string[] sentences;
+	[TextArea (1, 4)]
+	public string[] sentences;
 
 
 
